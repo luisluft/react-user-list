ed6ae0b49af668a37d317d0f47fea5331ddb7694
luisluft <luberluft@gmail.com>
2021-10-29 08:52:16 -0400

    add basic app structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import AddUser from "./components/users/AddUser";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <AddUser></AddUser>
+    </div>
+  );
 }
 
 export default App;
