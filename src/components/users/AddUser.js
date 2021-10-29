ed62a3225fa7892fe2976b85c61e368cb83b4aff
luisluft <luberluft@gmail.com>
2021-10-29 09:15:43 -0400

    implement username and age data storage

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../ui/Card";
 import classes from "./AddUser.module.css";
 import Button from "../ui/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log("enteredAge,enteredUsername :", enteredAge, enteredUsername);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
+        <input
+          type="text"
+          id="username"
+          value={enteredUsername}
+          onChange={userNameChangeHandler}
+        />
         <label htmlFor="age">Age (years)</label>
-        <input type="number" id="age" />
+        <input
+          type="number"
+          id="age"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
