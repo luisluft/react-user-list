fefd3307ab020742479074e04b9e98b4cb7d8cd9
luisluft <luberluft@gmail.com>
2021-10-30 12:15:19 -0400

    fix empty card showing up when there were no users on the list

--- src/components/users/UsersList.js
+++ src/components/users/UsersList.js
@@ -3,19 +3,21 @@ import Card from "../ui/Card";
 import classes from "./UsersList.module.css";
 
 const UsersList = (props) => {
-  return (
-    <Card className={classes.users}>
-      <ul>
-        {props.users.map((user) => {
-          return (
-            <li key={user.id}>
-              {user.name} ({user.age} years old)
-            </li>
-          );
-        })}
-      </ul>
-    </Card>
-  );
+  if (props.users.length !== 0)
+    return (
+      <Card className={classes.users}>
+        <ul>
+          {props.users.map((user) => {
+            return (
+              <li key={user.id}>
+                {user.name} ({user.age} years old)
+              </li>
+            );
+          })}
+        </ul>
+      </Card>
+    );
+  return 0;
 };
 
 export default UsersList;
